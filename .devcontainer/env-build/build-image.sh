faf5ce6e3ead4bdc6cab90d3c0e02c902b92ab5e
chaoxin.lu <chaoxin258@gmail.com>
2025-09-11 21:45:11 +0800

    添加dev containers

--- .devcontainer/env-build/build-image.sh
+++ .devcontainer/env-build/build-image.sh
@@ -2,11 +2,8 @@
 set -euo pipefail
 
 IMAGE_TAG="xin8/dev-env/node:20"
+PLATFORM=linux/amd64,linux/arm64/v8
 
-# 确保 buildx 可用并就绪
-if ! docker buildx ls >/dev/null 2>&1; then
-  docker buildx create --name devbuilder --use >/dev/null
-fi
 docker buildx inspect --bootstrap >/dev/null
 
 echo "使用平台: $PLATFORM 构建镜像: $IMAGE_TAG (no-cache)"
